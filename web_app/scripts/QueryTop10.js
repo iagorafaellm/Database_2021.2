b1a1294648dc18c70ef4c4cf09d70a2a5270af64
Lucas Araujo Carvalho <lucasaraujo180701@hotmail.com>
2022-03-07 19:52:08 -0300

    update: updating QueryTop10.js

--- web_app/scripts/QueryTop10.js
+++ web_app/scripts/QueryTop10.js
@@ -1,29 +1,11 @@
 //Query 1: Top 10 animes
 
 $.get("http://localhost:3000/top10", (result) => {
-        // var context = document.getElementsByClassName('data');
-        // const table = document.createElement("table");
-        // table.className = "topAnimes";
-        // const tableHeader = document.createElement("tr");
-        // tableHeader.innerHTML = "<td>Image</td><td>Rank</td><td>Anime ID</td><td>Title</td><td>Rating</td>";
-        // table.appendChild(tableHeader);
-        // context[0].appendChild(table);
-        
-        // context = document.getElementsByClassName('topAnimes');
-        // for(var i = 0; i < result[0].length; i++){
-        //     const tablerow = document.createElement("tr");
-        //     const animeImage = document.createElement("img");
-
-        //     tablerow.innerHTML = "<td></td>" + "<td>" + result[0][i] + "</td>" + "<td>" + result[1][i] + "</td>" + "<td>" + result[2][i] + "</td>" + "<td>" + result[3][i] + "</td>";
-        //     tablerow.firstChild.innerHTML = "<img src=" + result[4][i] + ">";
-
-        //     context[0].appendChild(tablerow)
-        // }
-
-
         var main = document.getElementsByTagName("main");
+        var rownum;
         
         for(var i = 0; i < result[0].length; i++){
+
             const animeCard = document.createElement("div");
             animeCard.className = "animeCard";
 
@@ -66,6 +48,12 @@ $.get("http://localhost:3000/top10", (result) => {
             container.appendChild(ranking);
             animeCard.appendChild(container);
 
-            main[0].appendChild(animeCard);
+            if(i%2 == 0){
+                rownum = document.createElement("div");
+                rownum.className = "line";
+            }
+
+            rownum.appendChild(animeCard)
+            main[0].appendChild(rownum);
         }
 });
